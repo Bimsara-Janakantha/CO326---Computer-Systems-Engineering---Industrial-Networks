f5949731fd2a9a31558f3f8f6d7352bc37db4734
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-12-17 19:33:41 +0530

    Refactor code and update character mapping for seven-segment display

--- Lab_01_Parallel_Port/P2_Count_0_to_9.c
+++ Lab_01_Parallel_Port/P2_Count_0_to_9.c
@@ -1,3 +1,20 @@
+/*
+Auther: Janakantha S.M.B.G. (E/20/157)
+Last Modified: 2025-12-17
+Title: Count from 0 to 9 on a Seven Segment Display using Parallel Port
+Description: This program counts from 0 to 9 on a seven-segment display connected to a parallel port.
+Note: Use a common anode seven-segment display.
+Port Map:
+    D0 -> a
+    D1 -> b
+    D2 -> c
+    D3 -> d
+    D4 -> e
+    D5 -> f
+    D6 -> g
+    D7 -> Common pin (Always HIGH)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,18 +25,18 @@
 
 // Character mapping
 unsigned char characters[] = {
-    0x3F, // 0
-    0x06, // 1
-    0x5B, // 2
-    0x4F, // 3
-    0x66, // 4
-    0x6D, // 5
-    0x7D, // 6
-    0x07, // 7
-    0x7F, // 8
-    0x67  // 9
+    0xC0, // 0
+    0xF9, // 1
+    0xA4, // 2
+    0xB0, // 3
+    0x99, // 4
+    0x92, // 5
+    0x82, // 6
+    0xF8, // 7
+    0x80, // 8
+    0x90  // 9
 };
-// Reference: https://hosteng.com/dmdhelp/content/instruction_set/SEG_Hex_BCD_to_7_Segment_Display.htm
+// Reference: https://www.electronicsforu.com/resources/7-segment-display-pinout-understanding
 
 // Variable to store write data
 unsigned char data;
@@ -41,11 +58,12 @@ void main()
 
         // Write to the parallel port
         outb(data, DATA_PORT);
+        printf("Displaying: %d -> Hex: %02X\n", i, data);
 
         // Wait 1 second
         sleep(1);
 
         // Increment the count
-        i = (i > 8) ? 0 : i + 1;
+        i = (i + 1) % 10;
     }
 }
