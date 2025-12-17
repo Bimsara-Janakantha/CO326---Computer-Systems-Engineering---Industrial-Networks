b0bc3f7afdc1adf112d5ee38610c641bd5c4d086
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-12-17 19:39:16 +0530

    Update documentation and correct binary mapping for seven-segment display

--- Lab_01_Parallel_Port/P3_Count_0_to_9_with_74LS47.c
+++ Lab_01_Parallel_Port/P3_Count_0_to_9_with_74LS47.c
@@ -1,3 +1,19 @@
+/*
+Auther: Janakantha S.M.B.G. (E/20/157)
+Last Modified: 2025-12-17
+Title: Count from 0 to 9 on a Seven Segment Display using Parallel Port with and a 74LS47 Decoder
+Description: This program counts from 0 to 9 on a seven-segment display connected to a parallel port.
+Note: Use a common anode seven-segment display.
+Port Map:
+    Parallel Port -> 74LS47 Decoder
+        D0        ->    A0
+        D1        ->    A1
+        D2        ->    A2
+        D3        ->    A3
+        D4        ->    VCC
+        GND       ->    GND
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,16 +24,16 @@
 
 // Binary mapping
 unsigned char binaries[] = {
-    0x00, // 0000
-    0x01, // 0001
-    0x02, // 0010
-    0x03, // 0011
-    0x04, // 0100
-    0x05, // 0101
-    0x06, // 0110
-    0x07, // 0111
-    0x08, // 1000
-    0x09  // 1001
+    0x10, // 0001 0000
+    0x11, // 0001 0001
+    0x12, // 0001 0010
+    0x13, // 0001 0011
+    0x14, // 0001 0100
+    0x15, // 0001 0101
+    0x16, // 0001 0110
+    0x17, // 0001 0111
+    0x18, // 0001 1000
+    0x19  // 0001 1001
 };
 
 // Variable to store write data
@@ -40,11 +56,12 @@ void main()
 
         // Write to the parallel port
         outb(data, DATA_PORT);
+        printf("Displaying: %d -> Input: %02X\n", i, data);
 
         // Wait 1 second
         sleep(1);
 
         // Increment the count
-        i = (i > 8) ? 0 : i + 1;
+        i = (i + 1) % 10;
     }
 }
